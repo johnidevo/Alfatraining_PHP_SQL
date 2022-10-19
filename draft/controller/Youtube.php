69d3f4746e693257b28c47c8cb361b93b2df07e1
Robert-Dan <danrobert445@gmail.com>
2022-10-19 07:19:59 +0000

    vendor folder

--- draft/controller/Youtube.php
+++ draft/controller/Youtube.php
@@ -4,8 +4,24 @@
 namespace Draft\Controller;
 
 class Youtube {
+	
+	public $sKey;
+
 	public function __construct(){
-		var_dump('__construct() Youtube');
+		$this->sKey = "AIzaSyBQnsqFY61VFgaOm1sdsRh_xlTIEwsyflI";
+	}
+
+	public function getItems(){
+		$client = new \Google\Client();
+		$client->setApplicationName("PHP/MySQL");
+		$client->setDeveloperKey($this->sKey);
+		$service = new \Google\Service\YouTube($client);
+	}
+	
+	public function dump($s) {
+		print '<pre>';
+		var_dump($s);
+		print '</pre>';
 	}
 }
 
