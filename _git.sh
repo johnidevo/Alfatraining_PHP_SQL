150cc1b97420d35637704f6aae5698397a6762ac
Robert-Dan <danrobert445@gmail.com>
2022-10-19 07:57:25 +0000

    default value

--- _git.sh
+++ _git.sh
@@ -7,8 +7,9 @@ git add .
 git status
 
 #"${FOO:=default value}"
-read -p "Enter your commit message: ${message:=default value}"
-git commit -m "${message}"
+read -p "Enter your commit message: " message
+
+git commit -m "${message:=default value}"
 
 
 #git remote add origin https://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@github.com/johnidevo/Alfatraining_PHP_SQL.git
