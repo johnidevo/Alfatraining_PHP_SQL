810825226df1e8dd35ca3a0eaa133ca0d3ce5d97
Robert-Dan <danrobert445@gmail.com>
2022-10-19 07:56:36 +0000

    default value

--- _git.sh
+++ _git.sh
@@ -1,12 +1,16 @@
+#!/bin/bash
+
 git config --global user.email "danrobert445@gmail.com"
 git config --global user.name "Robert-Dan"
 
 git add .
 git status
 
-read -p "Enter your commit message: " message
-
+#"${FOO:=default value}"
+read -p "Enter your commit message: ${message:=default value}"
 git commit -m "${message}"
+
+
 #git remote add origin https://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@github.com/johnidevo/Alfatraining_PHP_SQL.git
 git push origin master
 
