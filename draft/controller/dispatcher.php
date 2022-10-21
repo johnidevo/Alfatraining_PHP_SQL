d2db04168d8254c9aba4c0d69500a9bf3c0ae502
Robert-Dan <danrobert445@gmail.com>
2022-10-21 12:50:52 +0000

    ziel

--- draft/controller/dispatcher.php
+++ draft/controller/dispatcher.php
@@ -1,34 +1,12 @@
 <?php
-	
 #call dispatch
-
 #-router // get uri parameter for request
 #--call model // prepare material
 #---call system // process material
 #---setup frontend // update database
 #--call widget.php function to form the content // refere data
 #-call view // setup html document
-
 #end dispatcher // build
-
-/*
-$name = "Nathan";
-
-function greetings() {
-    global $name;
-    echo $name;
-    echo $name;
-    $name = "Joe";
-    echo $name;
-
-}
-
-//👇 prints Joe
-greetings();
-
-//👇 prints Joe as well
-echo $name;
-*/
 
 global $a_router_uri;
 
@@ -36,23 +14,18 @@ $a_router_uri = array();
 
 function dispatcher_dispatch()
 {
-	#global $a_router_uri;
-	
-	
-	var_dump($GLOBALS['a_router_uri']);
-	var_dump($GLOBALS);
-	$a_router_uri = array_merge($a_router_uri, 'dispatcher_dispatch');
+	global $a_router_uri;
+	$a_router_uri = array_merge($a_router_uri, array('dispatcher_dispatch'));
+
+	if (!router_get($a_router_uri)) error_throw('router_get()');
 	var_dump($a_router_uri);
 	return true;
 }
 
-if (!router_get()) error_throw('router_get()');
-
 if (!dispatcher_dispatch()) error_throw('dispatcher_dispatch()');
 
-
-
 var_dump($a_router_uri);
+
 #echo '<pre>';
 #var_dump($GLOBALS);
 #echo '</pre>';
--- draft/controller/router.php
+++ draft/controller/router.php
@@ -1,11 +1,10 @@
 <?php
 
-
 function router_get()
 {
 	global $a_router_uri;
 	#$a_router_uri = array_merge($a_router_uri, $_GET);
-	$a_router_uri = array_merge($a_router_uri, 'router_get');
+	$a_router_uri = array_merge($a_router_uri, array('router_get'));
 	var_dump($a_router_uri);
 	return true;
 }
