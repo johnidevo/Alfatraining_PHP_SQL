a3fcd82cc58aa9080d2ea13465014019e620cadd
Robert-Dan <danrobert445@gmail.com>
2022-10-21 10:09:44 +0000

    default value

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
 !defined('ROOT') && define('ROOT', __DIR__ .'/../');
-!defined('BIN') && define('BIN', ROOT .'/bin/');
-!defined('WWW') && define('WWW', ROOT .'/www/');
 !defined('DRAFT') && define('DRAFT', ROOT .'/draft/');
+!defined('VAR_SQL') && define('VAR_SQL', ROOT .'/var/sql/');
+!defined('WWW') && define('WWW', ROOT .'/www/');
 
 function autoload_custom()
 {
@@ -13,6 +13,5 @@ function autoload_custom()
 }
 
 autoload_custom();
-
 
 ?>
