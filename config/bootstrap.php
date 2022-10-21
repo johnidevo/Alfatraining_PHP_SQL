b9fde31d18b3a4e6fdf564b4ebd1bb72ad7de0eb
Robert-Dan <danrobert445@gmail.com>
2022-10-21 09:49:37 +0000

    bootstrap composer function

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,12 +1,18 @@
 <?php
 
-# PSR-4 autoload
-require(__DIR__ .'/../vendor/autoload.php');
-
 !defined('ROOT') && define('ROOT', __DIR__ .'/../');
 !defined('BIN') && define('BIN', ROOT .'/bin/');
 !defined('WWW') && define('WWW', ROOT .'/www/');
 !defined('DRAFT') && define('DRAFT', ROOT .'/draft/');
+
+# PSR-4 autoload //disabled
+#require(__DIR__ .'/../vendor/autoload.php');
+
+function autoload_custom() {
+	$sComposer = file_get_contents(ROOT .'composer.json');
+	var_dump($sComposer);
+}
+
 
 
 ?>
