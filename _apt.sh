877a4fc1ee86b62792fa6f38375c91b49f39a354
Robert-Dan <danrobert445@gmail.com>
2022-10-22 08:49:24 +0000

    sql problem

--- _apt.sh
+++ _apt.sh
@@ -1,1 +1,8 @@
-sudo apt-get install mysql-server
+#which php
+#php -i|grep 'php.ini'
+
+sudo apt-get install php7.3-mysqli
+
+sudo service apache2 restart
+
+
--- draft/model/frontend.php
+++ draft/model/frontend.php
@@ -1,44 +1,24 @@
 <?php
 
+
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'calendar');
+
+
 function frontend_init()
 {
 	var_dump('frontend_init');
+	if (!frontend_db_open()) error_throw('frontend_db_open()');
 	return true;
 }
 
-function dbOpen($sDb='')
+function frontend_db_open()
 {
-	//checks and valitations:
-	if(!defined('DB_ENGINE')) {
-		self::outputError('SQL connection type not defined!');
-		return FALSE;
-	}
-
-	if(!defined('DB_SERVER') || !defined('DB_USER') || !defined('DB_PASS') || !defined('DB_NAME')) {
-		self::outputError('Missing SQL Connection properties!');
-		return FALSE;
-	}
-
-	if($sDb == '') {
-		$sDb = DB_NAME;
-	}
-
-	if(!defined('DB_PORT')) {
-		define('DB_PORT', 3306);
-	}
-
-	self::$oc = @mysqli_connect(DB_SERVER, DB_USER, DB_PASS, $sDb, DB_PORT);
-		if (!self::$oc) {
-		self::outputError(' Error #'.mysqli_connect_errno().': Unable to connect to server:'.DB_SERVER.'<hr />MySql Server is down or connection is not properly configured. Please check the configuration of sql connection!<hr />'.mysqli_connect_error());
-	}
-}
-
-function dbClose()
-{
-	if(!mysqli_close(self::$oc)) {
-		self::outputError("Something went wrong while closing MySQL connection!");
-		return false;
-	}
+	$oMysql = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+	var_dump($oMysql);
+	return true;
 }
 
 
