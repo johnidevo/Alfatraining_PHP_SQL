e9740ed5be175e23f6cb92b4c9a820b91bd198a0
Robert-Dan <danrobert445@gmail.com>
2022-10-22 11:59:59 +0000

    default value

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-require('./www/index.php');
+!defined('ROOT') && define('ROOT', __DIR__ .'/');
+!defined('DRAFT') && define('DRAFT', ROOT .'/draft/');
+!defined('VAR_SQL') && define('VAR_SQL', ROOT .'/var/sql/');
+!defined('WWW_PUBLIC') && define('WWW_PUBLIC', ROOT .'/www/public/');
+
+function autoload_custom()
+{
+	$sComposer = file_get_contents(ROOT .'alfatraining.json');
+	$aComposer = json_decode($sComposer);
+	foreach($aComposer->autoload->files as $sFile) include(ROOT . $sFile);
+	return true;
+}
+
+if (!autoload_custom()) return print('autoload_custom()');
+if (!dispatcher_dispatch()) error_throw('dispatcher_dispatch()');
 ?>
