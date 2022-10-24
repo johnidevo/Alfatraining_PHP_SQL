254d7e4a7b20be091f642ea64ef9cfbefb4dbd55
Robert-Dan <danrobert445@gmail.com>
2022-10-24 13:25:18 +0000

    scheduler

--- draft/view/scheduler.php
+++ draft/view/scheduler.php
@@ -30,8 +30,8 @@ $sField = '';
 for ($i = $iDatePrevTable, $k=0; $i <= $iDateNextTable; $i = $i + 86400, $k++)
 {
 	if (date('m', time()) == date('m', $i)) 
-	$sField .= '<td><b>'. html_planer_radio_day(date('d', $i), $i) .'</b></td>';
-	else $sField .= '<td>'. html_planer_radio_day(date('d', $i), $i) .'</b></td>';
+	$sField .= '<td><b>'. html_scheduler_radio_day(date('d', $i), $i) .'</b></td>';
+	else $sField .= '<td>'. html_scheduler_radio_day(date('d', $i), $i) .'</b></td>';
 	if ($k == 6){
 		$k = -1;
 		$aPage['scheduler_content'] .= '<tr>'. $sField .'</tr>';
@@ -41,21 +41,6 @@ for ($i = $iDatePrevTable, $k=0; $i <= $iDateNextTable; $i = $i + 86400, $k++)
 $aPage['scheduler_content'] .= '</tbody>';
 $aPage['scheduler_content'] .= '</table>';
 
-
-/* Sidebar */
-$aPage['scheduler_sidebar'] .= '<table><thead><tr><td>Besprechungsstunde</td></tr></thead>';
-$aPage['scheduler_content'] .= '<tbody>';
-for ($i = 9; $i <= 15; $i++)
-{
-	$sField .= '<td>'. html_planer_radio_hour($i) .'</b></td>';
-	$aPage['scheduler_sidebar'] .= '<tr>'. $sField .'</tr>';
-	$sField = '';
-}
-$aPage['scheduler_content'] .= '</tbody>';
-$aPage['scheduler_sidebar'] .= '<tfoot><tr><td><input type="submit" value="Einreichen"></td></tr></tfoot>';
-$aPage['scheduler_sidebar'] .= '</table>';
-
-
 /*
 10 P	3. Daten anzeigen: Erstellen Sie einen รถffentlichen Bereich 
 		mit einer Anzeige.		
@@ -63,33 +48,17 @@ $aPage['scheduler_sidebar'] .= '</table>';
 $aPage['content'] .= '
 	<form action="/?'. http_build_query($aRouter) .'" method="post">
 		<div id="content">
-			<h3>Terminplaner</h3>
+			<h3>Scheduler liste</h3>
 			<hr></br>
 			'. $aPage['scheduler_content'] .'
-		</div>
-		<div id="sidebar">
-			<h3>Stunden auswählen</h3>
-			<hr></br>
-			'. $aPage['scheduler_sidebar'] .'
 		</div>
 	</form>
 ';
 
-function html_planer_radio_day($sName, $iValue){
-	return '<label for="date_'. $sName .'">'. $sName .'</label>
-	<input value="'. $iValue .'" type="radio" name="date_planer" id="date_'. $sName .'">';
+function html_scheduler_radio_day($sName, $iValue){
+	return '<label for="date_'. $sName .'">'. $sName .'</label>';
 }
 
-function html_planer_radio_hour($i){
-	return '<input value="'. str_pad($i, 2, 0, STR_PAD_LEFT) .':00:00" type="radio" name="hour_planer" id="hour_'. $i .'">
-	<label for="hour_'. $i .'">'. str_pad($i, 2, 0, STR_PAD_LEFT) .':00</label>';
-}
-
-function planer_init()
-{
-	global $sPage;
-	return true;
-}
 
 
 
