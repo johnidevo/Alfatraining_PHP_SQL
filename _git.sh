a898ead1cab1f55d36297c2163e001d1d8ed227d
Robert-Dan <danrobert445@gmail.com>
2022-10-25 07:12:26 +0000

    git

--- _git.sh
+++ _git.sh
@@ -12,7 +12,7 @@ read -p "Enter your commit message: " message
 git commit -m "${message:=default value}"
 
 
-#git remote add origin https://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@github.com/johnidevo/Alfatraining_PHP_SQL.git
+git remote add origin https://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@github.com/johnidevo/Alfatraining_PHP_SQL.git
 git push origin master
 
 #git pull origin master
